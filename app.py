7e71775774b456738d56e6f6c0501dac944796e0
Cheepati janardhan reddy(192211819) <124260818+janardhan1243@users.noreply.github.com>
2026-03-27 16:16:43 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ""Pipeline Working Perfectly ✅""
+    return "Pipeline Working Perfectly ✅"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
